242334e0af43f848296c5b0a3f9a29808d7affbd
Ilja Guzovski <ilja.guzovski@gmail.com>
2014-03-16 17:16:39 +0200

    angular hell

--- iDeloR&I/js/app.js
+++ iDeloR&I/js/app.js
@@ -1,12 +1,14 @@
-var iDeloApp = angular.module('iDeloApp', ['ngRoute','complaintsControllers' ]);
+var iDeloApp = angular.module('iDeloApp',
+		[ 'ngRoute', 'complaintsControllers' ]);
 
 iDeloApp.config([ '$routeProvider', function($routeProvider) {
-	$routeProvider.when('/allComplaints.html', {
-		controller : 'PhoneListCtrl'
-	}).when('/phones/:phoneId', {
-		templateUrl : 'partials/phone-detail.html',
-		controller : 'PhoneDetailCtrl'
+	$routeProvider.when('/allComplaints', {
+		templateUrl : 'allComplaints.html',
+		controller : 'ComplaintsCtrl'
+	}).when('/citizen', {
+		templateUrl : 'citizen.html',
+		controller : 'ComplaintsCtrl'
 	}).otherwise({
-		redirectTo : '/phones'
+		redirectTo : '/allComplaints'
 	});
 } ]);
