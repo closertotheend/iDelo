815c757f6fb5a8b91675a0a8f84ad0e72ac717ec
Ilja Guzovski <ilja.guzovski@gmail.com>
2014-03-17 09:39:00 +0200

    Minor improvements

--- iDeloR&I/js/app.js
+++ iDeloR&I/js/app.js
@@ -4,28 +4,23 @@ var iDeloApp = angular.module('iDeloApp',
 iDeloApp.config([ '$routeProvider', function($routeProvider) {
 	$routeProvider.when('/', {
 		templateUrl : 'indexInfo.html',
-		controller : 'ComplaintsCtrl'
 	}).when('/allComplaints', {
 		templateUrl : 'allComplaints.html',
 		controller : 'ComplaintsCtrl'
 	}).when('/allCitizens', {
 		templateUrl : 'allCitizens.html',
-		controller : 'ComplaintsCtrl'
+		controller : 'CitizensCtrl'
 	}).when('/myComplaints', {
 		templateUrl : 'myComplaints.html',
 		controller : 'ComplaintsCtrl'
 	}).when('/newComplaint', {
 		templateUrl : 'newComplaint.html',
-		controller : 'ComplaintsCtrl'
 	}).when('/citizen', {
 		templateUrl : 'citizen.html',
-		controller : 'ComplaintsCtrl'
 	}).when('/complaint', {
 		templateUrl : 'complaint.html',
-		controller : 'ComplaintsCtrl'
 	}).when('/search', {
 		templateUrl : 'search.html',
-		controller : 'ComplaintsCtrl'
 	}).otherwise({
 		redirectTo : '/allComplaints'
 	});
--- iDeloR&I/js/controllers.js
+++ iDeloR&I/js/controllers.js
@@ -5,7 +5,13 @@ iDeloApp.controller('ComplaintsCtrl', [ '$scope', '$http',
 			$http.get('json/allComplaints.json').success(function(data) {
 				$scope.complaints = data;
 			});
-
 			$scope.orderProp = 'type';
 		} ]);
 
+iDeloApp.controller('CitizensCtrl', [ '$scope', '$http',
+		function($scope, $http) {
+			$http.get('json/allCitizens.json').success(function(data) {
+				$scope.citizens = data;
+			});
+			$scope.orderProp = 'type';
+		} ]);
